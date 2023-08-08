67b5164dbd8a1539d9838af8306d210c15a77179
Brandon Foster <brandon.foster.vt@gmail.com>
2023-08-08 18:26:03 -0400

    development mode does not use React.StrictMode

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
+console.info(`This app is being run in [${process.env.NODE_ENV}] mode.`);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
+  {process.env.NODE_ENV === 'development' ?
     <App />
-  </React.StrictMode>
+    :
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  }
+  </>
 );
