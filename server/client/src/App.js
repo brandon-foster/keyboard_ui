bdacf4912397f9662bfe5704d9367a0e2dbb0fbd
Brandon Foster <brandon.foster.vt@gmail.com>
2023-08-09 14:46:29 -0400

    display a prompt using the paragraph element

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -4,15 +4,17 @@ import { appConf } from './conf/appConf';
 
 import { KeyboardWrapper } from './comp/KeyboardWrapper';
 import { UserInput } from './comp/UserInput';
+import { Prompt } from './comp/Prompt';
 
 function App() {
   return (
     <div className='App'>
       <header>
-        <h1>{appConf.APP_NAME}</h1>
+        <h1 className='App-h1'>{appConf.APP_NAME}</h1>
       </header>
       <main className='App-main'>
         <div className='App-componentContainer'>
+          <Prompt/>
           <UserInput/>
           <KeyboardWrapper/>
         </div>
---
+++ server/client/src/comp/Prompt.js
@@ -0,0 +1,9 @@
+import '../css/Prompt.css';
+
+export const Prompt = () => {
+    return (
+        <div className='Prompt'>
+            <p className='Prompt-p'>The quick brown fox jumps over the lazy dog.</p>
+        </div>
+    );
+};
